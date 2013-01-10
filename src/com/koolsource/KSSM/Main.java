deba8490df74d54fd96cbf9da1e85675336bee0e
Johnathan Chamberlain <jchamb2010@gmail.com>
2013-01-09 22:46:10 -0500

    Appending #030ea5b1

--- src/com/koolsource/KSSM/Main.java
+++ src/com/koolsource/KSSM/Main.java
@@ -175,29 +175,7 @@ public class Main extends JavaPlugin {
         return Main.httpUrl;
     }
 
-    public static String combineSplit(int startIndex, String[] string, String seperator) {
-        StringBuilder builder = new StringBuilder();
-
-        for (int i = startIndex; i < string.length; i++) {
-            builder.append(string[i]);
-            builder.append(seperator);
-        }
-
-        builder.deleteCharAt(builder.length() - seperator.length());
-        return builder.toString();
-    }
-
     public static void noPermission(Player aSender) {
         aSender.sendMessage(ChatColor.RED + "You do not have access to this command.");
-    }
-
-    public String[] arrayShift(String[] anArray, int position) {
-        String temp = anArray[position];
-        String[] array = null;
-
-        for (int i = (position - 1); i >= 0; i--) {
-            array[i + 1] = array[i];
-        }
-        return array;
     }
 }
